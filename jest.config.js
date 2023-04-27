44339f982b840bbb07c75b9ef5e1f9217dc1b805
Mike MacCana <mike.maccana@gmail.com>
2023-04-27 19:39:28 +0100

    Don't make occasionally slow network fail tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+
+const SECONDS = 1000;
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/*.test.ts"],
+  // Most of the time the services are <2 secs, but sometimes they're slow
+  // and we don't want to fail tests because of that.
+  testTimeout: 8 * SECONDS,
 };
